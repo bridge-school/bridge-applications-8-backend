d0256ee7d260931c5dc73e4fe1aafb7f98bbce9a
Shangni <shangnihu@gmail.com>
2019-09-08 19:48:58 -0400

    Added endpoint for cohorts data.

--- src/routes/cohorts/cohorts.controller.js
+++ src/routes/cohorts/cohorts.controller.js
@@ -11,7 +11,6 @@ exports.index = (req, res) => {
         })
       )
     .then(data => {
-      console.log(data);
       res.json({data});
     })
     .catch(err => console.error('Getting cohorts data error:, ', err));
