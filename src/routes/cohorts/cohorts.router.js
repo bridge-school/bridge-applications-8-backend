11b253abc44a93a0328fac2f8d63635b66d43764
Shangni Hu <shangnihu@gmail.com>
2019-09-17 19:14:04 -0400

    Set up backend single cohort endpoint. (#2)

--- src/routes/cohorts/cohorts.router.js
+++ src/routes/cohorts/cohorts.router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const cohortsController = require("./cohorts.controller");
 
 router.get('/', cohortsController.index);
+router.get('/:id', cohortsController.cohort);
 
 module.exports = {
   cohortsRouter: router
